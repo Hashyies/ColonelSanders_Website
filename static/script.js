b2952f905c6a91aed4f03826b6a7c0622cb82344
Hashyies Mahiss <shaddiyar2@gmail.com>
2023-09-09 14:07:35 +0000

    enoguh for today, tmr ill fix the features list

--- static/script.js
+++ static/script.js
@@ -1,10 +1,34 @@
 /* script.js */
 $(document).ready(function() {
+    var sections = $('.section');
+    var currentSection = 0;
+    var isAnimating = false;
+
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('#top-menu').addClass('scrolled');
-        } else {
-            $('#top-menu').removeClass('scrolled');
+        if (isAnimating) return;
+
+        var downTriggerPoint = $(window).height() * 0.2;
+        var upTriggerPoint = $(window).height() * 0.8;
+        var scrollPosition = $(window).scrollTop();
+
+        if (scrollPosition > $(sections[currentSection]).offset().top + downTriggerPoint) {
+            // Scroll down to next section if more than 20% through current section
+            currentSection++;
+            isAnimating = true;
+            $('html, body').animate({
+                scrollTop: $(sections[currentSection]).offset().top
+            }, 'slow', function() {
+                isAnimating = false;
+            });
+        } else if (currentSection > 0 && scrollPosition < $(sections[currentSection - 1]).offset().top + upTriggerPoint) {
+            // Scroll up to previous section if more than 80% through current section
+            currentSection--;
+            isAnimating = true;
+            $('html, body').animate({
+                scrollTop: $(sections[currentSection]).offset().top
+            }, 'slow', function() {
+                isAnimating = false;
+            });
         }
     });
 });
